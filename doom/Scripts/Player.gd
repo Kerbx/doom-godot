aa3e7a6dccc0b14bd31f2538eb64218ec222c59d
Kerbx <r3ostat@ya.ru>
2023-02-19 16:17:43 +0300

    Initial commit.

---
+++ doom/Scripts/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+
+var velocity = Vector3()
+var SPEED = 10
+var GRAVITY = -30
+
+
+func _ready():
+	pass
+
+
+func shoot():
+	pass
+	
+
+func getDamage(damageAmount):
+	pass
+	
+	
+func die():
+	pass
+	
+
+func changeGun(gun):
+	pass
