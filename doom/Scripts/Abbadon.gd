a1499e409e04883df169e5bdf03ff14fd2eb13a4
Kerbx <r3ostat@ya.ru>
2023-02-21 20:29:24 +0300

    Created pistol.

--- doom/Scripts/Abbadon.gd
+++ doom/Scripts/Abbadon.gd
@@ -7,12 +7,12 @@ onready var player = get_tree().get_nodes_in_group('player')[0]
 var path = []
 var pathIndex = 0
 var speed = 10
-var health = 100
+var health = 10
 
 
 func _ready():
 	pass
-	
+
 
 func _physics_process(delta):
 	if pathIndex < path.size():
@@ -21,22 +21,22 @@ func _physics_process(delta):
 			pathIndex += 1
 		else:
 			move_and_slide(direction.normalized() * speed, Vector3.UP)
-			
+
 	else:
 		findPath(player.global_transform.origin)
-	
-	
+
+
 func takeDamage(amount):
 	health -= amount
 	if health <= 0:
 		death()
-	
-	
+
+
 func findPath(target):
 	path = navigation.get_simple_path(global_transform.origin, target)
 	pathIndex = 0
-	
-	
+
+
 func death():
 	set_process(false)
 	set_physics_process(false)
@@ -45,7 +45,7 @@ func death():
 		$AnimatedSprite3D.play("die")
 	else:
 		$AnimatedSprite3D.play('die')
-	
-	
+
+
 func shoot(target):
 	pass
---
+++ doom/Scripts/Gun.gd
@@ -0,0 +1,31 @@
+extends Spatial
+
+
+onready var sprite = $CanvasLayer/Control/AnimatedSprite
+onready var rays = $Rays.get_children()
+
+var damage = 8
+var canShoot = true
+
+
+func _ready():
+	sprite.play("idle")
+
+
+func checkHit():
+	for ray in rays:
+		if ray.is_colliding():
+			if ray.get_collider().is_in_group("enemy"):
+				ray.get_collider().takeDamage(damage)
+
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("shoot") and canShoot:
+		sprite.play("shoot")
+		checkHit()
+		canShoot = false
+
+		yield (sprite, "animation_finished")
+
+		canShoot = true
+		sprite.play("idle")
