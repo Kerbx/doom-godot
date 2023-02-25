7d3741280f8c6b436400f97d37ae22e6c660556d
Kerbx <r3ostat@ya.ru>
2023-02-25 19:45:56 +0300

    Upgraded enemies.

--- doom/Scripts/Abbadon.gd
+++ doom/Scripts/Abbadon.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 
 onready var navigation = get_tree().get_nodes_in_group('navigation')[0]
 onready var player = get_tree().get_nodes_in_group('player')[0]
+onready var ray = $Visual
 
 var path = []
 var pathIndex = 0
@@ -10,19 +11,47 @@ export var speed = 10
 export var health = 10
 export var move = true
 
+
+var searching = false
+var shooting = false
+var dead = false
+
+
 func _ready():
 	pass
 
 
 func _physics_process(delta):
-	if pathIndex < path.size():
-		var direction = (path[pathIndex] - global_transform.origin)
-		if direction.length() < 1:
-			pathIndex += 1
+	if dead:
+		return
+	lookAtPlayer()
+	if searching and not shooting:
+		if pathIndex < path.size():
+			var direction = (path[pathIndex] - global_transform.origin)
+			if direction.length() < 1:
+				pathIndex += 1
+			else:
+				if move:
+					$AnimatedSprite3D.play("walking")
+					move_and_slide(direction.normalized() * speed * delta, Vector3.UP)
+	else:
+		if not shooting:
+			$AnimatedSprite3D.play("idle")
+
+
+func lookAtPlayer():
+	ray.look_at(player.global_transform.origin, Vector3.UP)
+	if ray.is_colliding():
+		if ray.get_collider().is_in_group("player"):
+			searching = true
+
 		else:
-			if move:
-				$AnimatedSprite3D.play("walking")
-				move_and_slide(direction.normalized() * speed * delta, Vector3.UP)
+			searching = false
+			var checkNear = $Ear.get_overlapping_bodies()
+
+			for body in checkNear:
+				if body.is_in_group("player"):
+					searching = true
 
 
 func takeDamage(amount):
@@ -42,6 +71,7 @@ func findPath(target):
 
 
 func death():
+	dead = true
 	set_process(false)
 	set_physics_process(false)
 	$CollisionShape.disabled = true
@@ -52,9 +82,26 @@ func death():
 
 	return
 
-func shoot(target):
-	pass
+func shoot():
+	if searching and not dead and not shooting:
+		$AnimatedSprite3D.play("attack")
+		shooting = true
+		yield ($AnimatedSprite3D, "frame_changed")
+		if ray.is_colliding():
+			if ray.get_collider().is_in_group("player"):
+				Global.changeHealth(-5)
+		yield ($AnimatedSprite3D, "animation_finished")
+		shooting = false
 
 
 func _on_Timer_timeout():
 	findPath(player.global_transform.origin)
+
+
+func _on_Ear_body_entered(body):
+	if body.is_in_group("player"):
+		searching = true
+
+
+func _on_ShootingTimer_timeout():
+	shoot()
